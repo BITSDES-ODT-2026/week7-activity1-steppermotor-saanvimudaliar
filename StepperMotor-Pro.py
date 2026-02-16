be3cad6ef01cfc2fbb89e80d6216db3a70515e98
saanvimudaliar <saanvimudaliar26@gmail.com>
2026-02-16 14:14:06 +0530

    Update StepperMotor-Pro.py

--- StepperMotor-Pro.py
+++ StepperMotor-Pro.py
@@ -1,1 +1,61 @@
+from machine import Pin
+import time
+
+in1 = Pin(5, Pin.OUT)
+in2 = Pin(12, Pin.OUT)
+in3 = Pin(25, Pin.OUT)
+in4 = Pin(19, Pin.OUT)
+
+steps = 512
+
+while True:
+    for i in range(steps):
+        in1.value(1)
+        in2.value(0)
+        in3.value(0)
+        in4.value(0)
+        time.sleep(0.005)
+        
+        in1.value(0)
+        in2.value(1)
+        in3.value(0)
+        in4.value(0)
+        time.sleep(0.005)
+        
+        in1.value(0)
+        in2.value(0)
+        in3.value(1)
+        in4.value(0)
+        time.sleep(0.005)
+        
+        in1.value(0)
+        in2.value(0)
+        in3.value(0)
+        in4.value(1)
+        time.sleep(0.005)
+    
+    for i in range(steps):
+        in1.value(0)
+        in2.value(0)
+        in3.value(0)
+        in4.value(1)
+        time.sleep(0.005)
+        
+        in1.value(0)
+        in2.value(0)
+        in3.value(1)
+        in4.value(0)
+        time.sleep(0.005)
+        
+        in1.value(0)
+        in2.value(1)
+        in3.value(0)
+        in4.value(0)
+        time.sleep(0.005)
+        
+        in1.value(1)
+        in2.value(0)
+        in3.value(0)
+        in4.value(0)
+        time.sleep(0.005)
 
